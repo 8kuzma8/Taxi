0b7971a4668ac8a29a9954dc15bf977322c553de
Kuzma <kuzma.insarov11@gmail.com>
2023-12-17 16:37:49 +0200

    add tests

--- TaskManager/main/tests.py
+++ TaskManager/main/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+# main/tests.py
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Car, Order
+
+class OrderViewTest(TestCase):
+    def test_order_taxi_view(self):
+        response = self.client.get(reverse('order_taxi'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'order_taxi.html')
+
+    def test_order_taxi_post(self):
+        car = Car.objects.create(car_class="Economy")
+        data = {
+            'pickup_location': 'A',
+            'destination': 'B',
+            'pickup_time': '2023-12-31T12:00:00',
+            'car': car.id,
+        }
+        response = self.client.post(reverse('order_taxi'), data)
+        self.assertEqual(response.status_code, 302)  # 302 is a redirect status code
+        self.assertEqual(Order.objects.count(), 1)
